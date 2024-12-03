572cdca40cb60d03ec342eca4c2c45e1680f0dc9
Raphael Thesmar <thesmarraphael@gmail.com>
2024-12-02 23:28:15 -0500

    Quick last updates before Zzzzzz

--- bash_scripts/train_vit_lora.sh
+++ bash_scripts/train_vit_lora.sh
@@ -6,7 +6,7 @@ PYTHON_SCRIPT="ViT_training.py"
 # Define dataset to be used
 DATASET_1="stanford-dogs"
 DATASET_2="oxford-pet"
-DATASET=$DATASET_2
+DATASET=$DATASET_1
 
 # Define number of classes combinations and total number of classes
 if [ "$DATASET" = "$DATASET_1" ]; then
