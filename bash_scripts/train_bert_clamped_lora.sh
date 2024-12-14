ad3e3ab1d15dd21bf04f214a70cb7dba0b4689b9
Raphael Thesmar <thesmarraphael@gmail.com>
2024-12-14 15:41:50 -0500

    More results

--- bash_scripts/train_bert_clamped_lora.sh
+++ bash_scripts/train_bert_clamped_lora.sh
@@ -30,7 +30,7 @@ for i in {1..10}; do
         random_classes=$(generate_random_classes $num_classes)
                 
         # Run the Python script with the generated classes
-        python $PYTHON_SCRIPT --dataset $DATASET --classes $random_classes --use_lora --clamp_min_classes $min_classes
+        python $PYTHON_SCRIPT --dataset $DATASET --classes $random_classes --use_lora --lora_rank 768 --clamp_min_classes $min_classes
         
         # Add a small delay to avoid overlaps (optional)
         sleep 1
--- bash_scripts/train_shuffled_lora.sh
+++ bash_scripts/train_shuffled_lora.sh
@@ -7,7 +7,7 @@ for shuffle_proportion in 0.05 0.1 0.15 0.2 0.3 0.4 0.5; do
     # Run the Python scripts
     python ViT_training.py --dataset oxford-pet --use_lora --shuffle_label_ratio $shuffle_proportion
     python ViT_training.py --dataset stanford-dogs --use_lora --shuffle_label_ratio $shuffle_proportion
-    python BERT_training.py --dataset arxiv --use_lora --shuffle_label_ratio $shuffle_proportion
+    python BERT_training.py --dataset arxiv --use_lora --shuffle_label_ratio $shuffle_proportion --classes 132 12 50 101 38 34 79 49 84 59 20 1 95 118 37 48 59 95 51 78
     
     # Add a small delay to avoid overlaps (optional)
     sleep 1
